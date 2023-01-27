c4f6d31b7055ee7f36c0e480af0512f05c7c8e88
joel <joeldowono1@gmail.com>
2023-01-27 14:35:38 +0100

    Fonction AfficherMot

---
+++ jeu_du_pendu/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void AfficherMot(string mot, List<char> lettres)
+        {
+            for(int i = 0; i < mot.Length; i++)
+            {
+                char lettre = mot[i];
+                if(lettres.Contains(lettre))
+                {
+                    Console.Write(lettre + " ");
+                }
+                else
+                {
+                    Console.Write("_ ");
+                } 
+            }
+            Console.WriteLine();
+        }
+
+        static void DevinerMot(string mot)
+        {
+            //AfficherMot()
+
+            //DemanderUneLettre()
+        }
+        static void Main(string[] args)
+        {
+            string mot = "ELEPHANT";
+
+            AfficherMot(mot, new List<char> { 'E', 'L', 'w' });
+
+            //DevinerMot(mot);
+        }
+    }
+}
